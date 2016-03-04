51a28eccae857ceaa8f80051667740403fb7f1a3
qianfeng <arh@tianfy.local>
2016-03-04 09:50:34 +0800

    这是第一次提交

---
+++ ChatGit/ChatGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ChatGit
+//
+//  Created by arh on 16/3/4.
+//  Copyright © 2016年 阿茹汗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ChatGit/ChatGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChatGit
+//
+//  Created by arh on 16/3/4.
+//  Copyright © 2016年 阿茹汗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
